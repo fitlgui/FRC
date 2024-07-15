59a04ac4f3b30ef6e506961e73c8ea811a01cd84
Guilherme Fitl De Carvalho <135091807+fitlgui@users.noreply.github.com>
2024-07-15 17:05:19 -0400

    FRC PONTUACAO CORRIGIDA

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 const rangeInputafautonomo = document.getElementById('rangeafautonomo');
 const rangeValueafautonomo = document.getElementById('afautonomo');
-
 const rangeInputampautonomo = document.getElementById('rangeampautonomo');
-const rangeValueampautonomo= document.getElementById('ampautonomo');
+const rangeValueampautonomo = document.getElementById('ampautonomo');
 
 const rangeInputaf = document.getElementById('rangeaf');
 const rangeValueaf = document.getElementById('af');
@@ -13,67 +12,8 @@ const rangeValueafc = document.getElementById('afc');
 const rangeInputapf = document.getElementById('rangeapf');
 const rangeValueapf = document.getElementById('apf');
 
-// 
-const totalValueSpan = document.getElementById('totalValue');
-let totalValue = parseInt(totalValueSpan.textContent, 0);
-
-function updateTotalValue() {
-    totalValue = parseInt(rangeInputafautonomo.value) +
-                 parseInt(rangeInputampautonomo.value) +
-                 parseInt(rangeInputaf.value) +
-                 parseInt(rangeInputafc.value) +
-                 parseInt(rangeInputapf.value);
-    totalValueSpan.textContent = 'Pontos: '+totalValue;
-}
-
-rangeInputafautonomo.addEventListener('input', function () {
-    rangeValueafautonomo.textContent = this.value;
-    updateTotalValue();
-});
-
-rangeInputampautonomo.addEventListener('input', function () {
-    rangeValueampautonomo.textContent = this.value;
-    updateTotalValue();
-});
-
-rangeInputaf.addEventListener('input', function () {
-    rangeValueaf.textContent = this.value;
-    updateTotalValue();
-});
-
-rangeInputafc.addEventListener('input', function () {
-    rangeValueafc.textContent = this.value;
-    updateTotalValue();
-});
-
-rangeInputapf.addEventListener('input', function () {
-    rangeValueapf.textContent = this.value;
-    updateTotalValue();
-});
-// 
-
-rangeInputafautonomo.addEventListener('input', function () {
-    rangeValueafautonomo.textContent = this.value;
-});
-
-rangeInputampautonomo.addEventListener('input', function () {
-    rangeValueampautonomo.textContent = this.value;
-});
-
-rangeInputaf.addEventListener('input', function () {
-    rangeValueaf.textContent = this.value;
-});
-
-rangeInputafc.addEventListener('input', function () {
-    rangeValueafc.textContent = this.value;
-});
-
-rangeInputapf.addEventListener('input', function () {
-    rangeValueapf.textContent = this.value;
-});
-/////////////////////////////
-const checksaida = document.getElementById('checksaida');
 const labelsaida = document.getElementById('labelsaida');
+const checksaida = document.getElementById('checksaida');
 
 const checkcorrente = document.getElementById('checkcorrente');
 const labelcorrente = document.getElementById('labelcorrente');
@@ -81,44 +21,119 @@ const labelcorrente = document.getElementById('labelcorrente');
 const checktrap = document.getElementById('checktrap');
 const labeltrap = document.getElementById('labeltrap');
 
-const checkbox = document.getElementById('checkmic');
-const label = document.getElementById('labelmic');
+const checkmic = document.getElementById('checkmic');
+const labelmic = document.getElementById('labelmic');
+
+let zona = 0;
+let corrente = 0;
+let trap = 0;
+let mic = 0;
+let coop = 0;
 
 checksaida.addEventListener('change', function () {
     if (this.checked) {
         labelsaida.textContent = 'Sim';
+        zona = 2;
     } else {
         labelsaida.textContent = 'Não';
+        zona = 0;
     }
+    updateTotalValue();
 });
 
 checkcorrente.addEventListener('change', function () {
     if (this.checked) {
         labelcorrente.textContent = 'Sim';
+        corrente = 1;
     } else {
         labelcorrente.textContent = 'Não';
+        corrente = 0;
     }
+    updateTotalValue();
 });
 
 checktrap.addEventListener('change', function () {
     if (this.checked) {
         labeltrap.textContent = 'Sim';
+        trap = 5;
     } else {
         labeltrap.textContent = 'Não';
+        trap = 0;
     }
+    updateTotalValue();
 });
 
-checkbox.addEventListener('change', function () {
+checkmic.addEventListener('change', function () {
     if (this.checked) {
-        label.textContent = 'Sim';
+        labelmic.textContent = 'Sim';
+        mic = 1;
     } else {
-        label.textContent = 'Não';
+        labelmic.textContent = 'Não';
+        mic = 0;
     }
+    updateTotalValue();
+});
+
+checkcoop.addEventListener('change', function () {
+    if (this.checked) {
+        labelcoop.textContent = 'Sim';
+        coop = 1;
+    } else {
+        labelcoop.textContent = 'Não';
+        coop = 0;
+    }
+    updateTotalValue();
 });
 
 // Set initial label text based on checkbox state
-if (checkbox.checked) {
-    label.textContent = 'Sim';
+if (checkmic.checked) {
+    labelmic.textContent = 'Sim';
 } else {
-    label.textContent = 'Não';
+    labelmic.textContent = 'Não';
 }
+
+// Valor Total 
+const totalValueSpan = document.getElementById('totalValue');
+let totalValue = 0;
+
+function updateTotalValue() {
+    totalValue = parseInt((rangeInputafautonomo.value) * 5) +
+                 parseInt((rangeInputampautonomo.value) * 2) +
+                 parseInt((rangeInputaf.value) * 2) +
+                 parseInt((rangeInputafc.value) * 5) +
+                 parseInt(rangeInputapf.value) +
+                 parseInt(trap) +
+                 parseInt(corrente) +
+                 parseInt(zona) +
+                 parseInt(mic);
+    if (coop != 0){
+    totalValueSpan.textContent = 'Pontos: ' + totalValue + ' e 1 Coop';
+    } else {
+        totalValueSpan.textContent = 'Pontos: ' + totalValue;
+    }
+}
+
+rangeInputafautonomo.addEventListener('input', function () {
+    rangeValueafautonomo.textContent = this.value;
+    updateTotalValue();
+});
+
+rangeInputampautonomo.addEventListener('input', function () {
+    rangeValueampautonomo.textContent = this.value;
+    updateTotalValue();
+});
+
+rangeInputaf.addEventListener('input', function () {
+    rangeValueaf.textContent = this.value;
+    updateTotalValue();
+});
+
+rangeInputafc.addEventListener('input', function () {
+    rangeValueafc.textContent = this.value;
+    updateTotalValue();
+});
+
+rangeInputapf.addEventListener('input', function () {
+    rangeValueapf.textContent = this.value;
+    updateTotalValue();
+});
