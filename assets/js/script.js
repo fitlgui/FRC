219018bb450df57c2738382b05a4e84275fcac38
Guilherme Fitl De Carvalho <135091807+fitlgui@users.noreply.github.com>
2024-07-17 17:33:23 -0400

    Código com Interligação em API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,70 @@
+// Selecionar os elementos do DOM
+
+// Auto Falante Autonomo
 const rangeInputafautonomo = document.getElementById('rangeafautonomo');
 const rangeValueafautonomo = document.getElementById('afautonomo');
+
+// Amplificador Autonomo
 const rangeInputampautonomo = document.getElementById('rangeampautonomo');
 const rangeValueampautonomo = document.getElementById('ampautonomo');
 
-const equipe = document.getElementById('equipe')
+// Numero da Equipe
+const equipe = document.getElementById('equipe');
 
+// Auto Falante no Tele-operado
 const rangeInputaf = document.getElementById('rangeaf');
 const rangeValueaf = document.getElementById('af');
 
+// Auto Falante Amplificado no Tele-Op
 const rangeInputafc = document.getElementById('rangeafc');
 const rangeValueafc = document.getElementById('afc');
 
+// Amplificador no Tele-Op
 const rangeInputapf = document.getElementById('rangeapf');
 const rangeValueapf = document.getElementById('apf');
 
+// Saida da Zona
 const labelsaida = document.getElementById('labelsaida');
 const checksaida = document.getElementById('checksaida');
 
+// Corrente
 const checkcorrente = document.getElementById('checkcorrente');
 const labelcorrente = document.getElementById('labelcorrente');
 
+// Estacionou em Baixo do Palco
 const checkestacionou = document.getElementById('checkestacionou');
 const labelestacionou = document.getElementById('labelestacionou');
 
+// Trap - Palco
 const checktrap = document.getElementById('checktrap');
 const labeltrap = document.getElementById('labeltrap');
 
+// Microfone - Palco
 const checkmic = document.getElementById('checkmic');
 const labelmic = document.getElementById('labelmic');
 
+// Coopertition
+const checkcoop = document.getElementById('checkcoop');
+const labelcoop = document.getElementById('labelcoop');
+
+// Botão de Enviar
 const btnEnviar = document.getElementById('enviar');
 
+// Variáveis Mutaveis
 let zona = 0;
 let corrente = 0;
 let trap = 0;
 let mic = 0;
 let coop = 0;
 let estacionou = 0;
+let conteudo = document.getElementById('body');
+
+// Checagem de Botões de Verificação - Saida da Zona - Coopertition - Microfone - Trap - Corrente - Estacionou
 
 checksaida.addEventListener('change', function () {
     if (this.checked) {
         labelsaida.textContent = 'Sim';
-        zona = 2;
+        zona = 2; // Pontuação Zona
     } else {
         labelsaida.textContent = 'Não';
         zona = 0;
@@ -61,11 +84,15 @@ checkcorrente.addEventListener('change', function () {
 });
 
 checkestacionou.addEventListener('change', function () {
-    if (this.checked) {
-        labelcorrente.textContent = 'Sim';
+    if (corrente == 1) {
+        this.checked = true;
+        labelestacionou.textContent = 'Sim';
+        estacionou = 1;
+    } else if (this.checked) {
+        labelestacionou.textContent = 'Sim';
         estacionou = 1;
     } else {
-        labelcorrente.textContent = 'Não';
+        labelestacionou.textContent = 'Não';
         estacionou = 0;
     }
     updateTotalValue();
@@ -104,7 +131,7 @@ checkcoop.addEventListener('change', function () {
     updateTotalValue();
 });
 
-// Set initial label text based on checkbox state
+// 
 if (checkmic.checked) {
     labelmic.textContent = 'Sim';
 } else {
@@ -122,12 +149,12 @@ function updateTotalValue() {
                  parseInt((rangeInputafc.value) * 5) +
                  parseInt(rangeInputapf.value) +
                  parseInt(trap) +
-                 parseInt(estacionou)+
+                 parseInt(estacionou) +
                  parseInt(corrente) +
                  parseInt(zona) +
                  parseInt(mic);
-    if (coop != 0){
-    totalValueSpan.textContent = 'Pontos: ' + totalValue + ' e 1 Coop';
+    if (coop != 0) {
+        totalValueSpan.textContent = 'Pontos: ' + totalValue + ' e 1 Coop';
     } else {
         totalValueSpan.textContent = 'Pontos: ' + totalValue;
     }
@@ -159,26 +186,26 @@ rangeInputapf.addEventListener('input', function () {
 });
 
 // Enviar Dados
-
 async function sendData() {
     const data = {
-        equipe: parseInt(equipe.value),
-        rangeInputafautonomo: parseInt(rangeInputafautonomo.value),
-        rangeInputampautonomo: parseInt(rangeInputampautonomo.value),
-        rangeInputaf: parseInt(rangeInputaf.value),
-        rangeInputafc: parseInt(rangeInputafc.value),
-        rangeInputapf: parseInt(rangeInputapf.value),
-        zona,
-        corrente,
-        trap,
-        mic,
-        coop,
-        estacionou,
-        totalValue
+        equipe: equipe.value, // Número da Equipe
+        rangeInputafautonomo: parseInt(rangeInputafautonomo.value), // Notas no Auto-Falante no Autonomo
+        rangeInputampautonomo: parseInt(rangeInputampautonomo.value), // Notas no Amplificador no Autonomo
+        rangeInputaf: parseInt(rangeInputaf.value), // Notas no Auto-Falante Tele-Operado
+        rangeInputafc: parseInt(rangeInputafc.value), // Notas Amplificadas no Auto-Falante no Tele-Operado
+        rangeInputapf: parseInt(rangeInputapf.value), // Notas no Amplificador
+        zona, // Se Saiu da Zona
+        corrente, // Se fez corrente
+        trap, // Se fez trap
+        mic, // Se fez microfone
+        coop, // Se fez coopertition
+        estacionou, // Se estacionou
+        totalValue // Pontos Totais
     };
 
     try {
         const response = await fetch('http://localhost:3000/data', {
+            // Passando Dados em json para a API
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -186,27 +213,22 @@ async function sendData() {
             body: JSON.stringify(data),
         });
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error('Erro ao enviar os dados');
         }
         const result = await response.json();
-        console.log('Data sent successfully:', result);
+        console.log('Enviado com sucesso:', result);
+        conteudo.innerHTML = '<div class="alert alert-info" role="alert"> Dados Enviados Com Sucesso </div>'
+        // Recarregar a página após o envio bem-sucedido
+        setTimeout(() => {window.location.reload();}, 5000);
     } catch (error) {
-        console.error('Error sending data:', error);
+        console.error('Deu ruim ao enviar:', error);
+        conteudo.innerHTML = '<div class="alert alert-danger" role="alert">Erro Ao Enviar, Tente Novamente!</div>';
+        // Recarregar a página após o erro de envio
+        setTimeout(() => {window.location.reload();}, 5000);
     }
 }
 
-btnEnviar.addEventListener('click', () => {
-    equipe.addEventListener('change', sendData);
-    checksaida.addEventListener('change', sendData);
-    checkcorrente.addEventListener('change', sendData);
-    checkestacionou.addEventListener('change', sendData);
-    checktrap.addEventListener('change', sendData);
-    checkmic.addEventListener('change', sendData);
-    checkcoop.addEventListener('change', sendData);
-    rangeInputafautonomo.addEventListener('input', sendData);
-    rangeInputampautonomo.addEventListener('input', sendData);
-    rangeInputaf.addEventListener('input', sendData);
-    rangeInputafc.addEventListener('input', sendData);
-    rangeInputapf.addEventListener('input', sendData);
-    console.log('Dados Enviados');
-}); 
+btnEnviar.addEventListener('click', (event) => {
+    event.preventDefault();
+    sendData();
+});
