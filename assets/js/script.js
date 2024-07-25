1afc0a11dfaf899cd53c2bdb6fc6693140662772
Guilherme Fitl De Carvalho <135091807+fitlgui@users.noreply.github.com>
2024-07-25 16:10:21 -0400

    Front End - 1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,38 @@
-// Equipe
+// Elementos da DOM
 const equipe = document.getElementById('equipe');
 const partida = document.getElementById('partida');
+const alianca = document.getElementById('alianca');
+const resultado = document.getElementById('resultado');
 
-// Aliança
-const alianca = document.getElementById('alianca')
-
-// Escolha - Notas Auto-Falante / Autônomo
 const rangeInputafautonomo = document.getElementById('rangeafautonomo');
 const rangeValueafautonomo = document.getElementById('afautonomo');
-
-// Escolha - Notas Amplificador / Autônomo
 const rangeInputampautonomo = document.getElementById('rangeampautonomo');
 const rangeValueampautonomo = document.getElementById('ampautonomo');
-
-// Escolha - Notas Auto-Falante / Tele-OP
 const rangeInputaf = document.getElementById('rangeaf');
 const rangeValueaf = document.getElementById('af');
-
-// Escolha - Notas Auto-Falante Amplificado / Tele-OP
 const rangeInputafc = document.getElementById('rangeafc');
 const rangeValueafc = document.getElementById('afc');
-
-// Escolha - Notas Amplificador / Tele-OP
 const rangeInputapf = document.getElementById('rangeapf');
 const rangeValueapf = document.getElementById('apf');
 
-// Botão Saida Da Zona
 const labelsaida = document.getElementById('labelsaida');
 const checksaida = document.getElementById('checksaida');
-
-// Botão Corrente
 const checkcorrente = document.getElementById('checkcorrente');
 const labelcorrente = document.getElementById('labelcorrente');
-
-// Botão Estacionou
 const checkestacionou = document.getElementById('checkestacionou');
 const labelestacionou = document.getElementById('labelestacionou');
-
-// Botão Trap
 const checktrap = document.getElementById('checktrap');
 const labeltrap = document.getElementById('labeltrap');
-
-// Botão Microfone
 const checkmic = document.getElementById('checkmic');
 const labelmic = document.getElementById('labelmic');
-
-// Botão Coopertition
 const checkcoop = document.getElementById('checkcoop');
 const labelcoop = document.getElementById('labelcoop');
-
-// Botão de Enviar Dados
 const btnEnviar = document.getElementById('enviar');
 
+const totalValueSpan = document.getElementById('totalValue');
+const erro = document.getElementById('erro');
+const sucesso = document.getElementById('sucesso');
+const Key = document.getElementById('key');
 
 // Variáveis de pontuação
 let zona = 0;
@@ -61,117 +42,63 @@ let mic = 0;
 let coop = 0;
 let estacionou = 0;
 
-// Pop - Up
-let erro = document.getElementById('erro');
-let sucesso = document.getElementById('sucesso');
+// Função para atualizar o valor total
+function updateTotalValue() {
+    let totalValue = parseInt(rangeInputafautonomo.value) * 5 +
+                     parseInt(rangeInputampautonomo.value) * 2 +
+                     parseInt(rangeInputaf.value) * 2 +
+                     parseInt(rangeInputafc.value) * 5 +
+                     parseInt(rangeInputapf.value) +
+                     trap + corrente + estacionou + zona + mic;
+    totalValueSpan.textContent = coop !== 0 ? `Pontos: ${totalValue} e 1 Coop` : `Pontos: ${totalValue}`;
+}
 
-// Segurança
-let Key = document.getElementById('key');
-
-// Verificação para ver se botões estão selecionados
-
+// Funções de manipulação dos checkboxes
 checksaida.addEventListener('change', function () {
-    if (this.checked) {
-        labelsaida.textContent = 'Sim';
-        zona = 2;
-    } else {
-        labelsaida.textContent = 'Não';
-        zona = 0;
-    }
+    zona = this.checked ? 2 : 0;
+    labelsaida.textContent = this.checked ? 'Sim' : 'Não';
     updateTotalValue();
 });
 
 checkcorrente.addEventListener('change', function () {
+    corrente = this.checked ? 1 : 0;
+    labelcorrente.textContent = this.checked ? 'Sim' : 'Não';
     if (this.checked) {
-        labelcorrente.textContent = 'Sim';
-        corrente = 1;
-        labelestacionou.textContent = 'Sim';
         estacionou = 1;
         checkestacionou.checked = true;
     } else {
-        labelcorrente.textContent = 'Não';
-        labelestacionou.textContent = 'Não';
-        corrente = 0;
         estacionou = 0;
         checkestacionou.checked = false;
     }
+    labelestacionou.textContent = estacionou ? 'Sim' : 'Não';
     updateTotalValue();
 });
 
 checkestacionou.addEventListener('change', function () {
-    if (this.checked) {
-        labelestacionou.textContent = 'Sim';
-        estacionou = 1;
-    } else {
-        labelestacionou.textContent = 'Não';
-        estacionou = 0;
-    }
+    estacionou = this.checked ? 1 : 0;
+    labelestacionou.textContent = this.checked ? 'Sim' : 'Não';
     updateTotalValue();
 });
 
 checktrap.addEventListener('change', function () {
-    if (this.checked) {
-        labeltrap.textContent = 'Sim';
-        trap = 5;
-    } else {
-        labeltrap.textContent = 'Não';
-        trap = 0;
-    }
+    trap = this.checked ? 5 : 0;
+    labeltrap.textContent = this.checked ? 'Sim' : 'Não';
     updateTotalValue();
 });
 
 checkmic.addEventListener('change', function () {
-    if (this.checked) {
-        labelmic.textContent = 'Sim';
-        mic = 1;
-    } else {
-        labelmic.textContent = 'Não';
-        mic = 0;
-    }
+    mic = this.checked ? 1 : 0;
+    labelmic.textContent = this.checked ? 'Sim' : 'Não';
     updateTotalValue();
 });
 
 checkcoop.addEventListener('change', function () {
-    if (this.checked) {
-        labelcoop.textContent = 'Sim';
-        coop = 1;
-    } else {
-        labelcoop.textContent = 'Não';
-        coop = 0;
-    }
+    coop = this.checked ? 1 : 0;
+    labelcoop.textContent = this.checked ? 'Sim' : 'Não';
     updateTotalValue();
 });
 
-
-// Set initial label text based on checkbox state
-if (checkmic.checked) {
-    labelmic.textContent = 'Sim';
-} else {
-    labelmic.textContent = 'Não';
-}
-
-// Valor Total 
-const totalValueSpan = document.getElementById('totalValue');
-let totalValue = 0;
-
-function updateTotalValue() {
-    totalValue = parseInt((rangeInputafautonomo.value) * 5) +
-                 parseInt((rangeInputampautonomo.value) * 2) +
-                 parseInt((rangeInputaf.value) * 2) +
-                 parseInt((rangeInputafc.value) * 5) +
-                 parseInt(rangeInputapf.value) +
-                 parseInt(trap) +
-                 parseInt(estacionou) +
-                 parseInt(corrente) +
-                 parseInt(zona) +
-                 parseInt(mic);
-    if (coop != 0){
-        totalValueSpan.textContent = 'Pontos: ' + totalValue + ' e 1 Coop';
-    } else {
-        totalValueSpan.textContent = 'Pontos: ' + totalValue;
-    }
-}
-
+// Atualizar valores dos ranges
 rangeInputafautonomo.addEventListener('input', function () {
     rangeValueafautonomo.textContent = this.value;
     updateTotalValue();
@@ -197,49 +124,62 @@ rangeInputapf.addEventListener('input', function () {
     updateTotalValue();
 });
 
-// Enviar Dados
-    async function sendData() {
-        const data = {
-            equipe: equipe.value,
-            partida: partida.value,
-            rangeInputafautonomo: parseInt(rangeInputafautonomo.value),
-            rangeInputampautonomo: parseInt(rangeInputampautonomo.value),
-            rangeInputaf: parseInt(rangeInputaf.value),
-            rangeInputafc: parseInt(rangeInputafc.value),
-            rangeInputapf: parseInt(rangeInputapf.value),
-            zona,
-            corrente,
-            trap,
-            mic,
-            coop,
-            estacionou,
-            alianca,
-            Key,
-            totalValue
-        };
-
-        try {
-            const response = await fetch('https://scout-mt.onrender.com/data', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'x-api-key': 'PAOCOMCARNE123$',
-                    'key': Key.value
-                },
-                body: JSON.stringify(data),
-            });
-            if (!response.ok) {
-                throw new Error('Erro de internet');        
-            }
-            const result = await response.json();
-            console.log('Enviado com sucesso:', result);
-            sucesso = sucesso.show();
-        } catch (error) {
-            console.error('Deu ruim ao enviar:', error);
-            erro = erro.show();   
-        }    setTimeout(() => {
-            location.reload();
-        }, 7000) // Recarregar a página após envio
+// Função para enviar dados
+async function sendData() {
+    let rankingPoints = 0;
+    if (resultado.textContent === 'Vitória') {
+        rankingPoints = 2;
+    } else if (resultado.textContent === 'Empate') {
+        rankingPoints = 1;
     }
-    
-    btnEnviar.addEventListener('click', sendData);
+
+    const data = {
+        equipe: equipe.value,
+        partida: parseInt(partida.value),
+        rangeInputafautonomo: parseInt(rangeInputafautonomo.value),
+        rangeInputampautonomo: parseInt(rangeInputampautonomo.value),
+        rangeInputaf: parseInt(rangeInputaf.value),
+        rangeInputafc: parseInt(rangeInputafc.value),
+        rangeInputapf: parseInt(rangeInputapf.value),
+        zona,
+        corrente,
+        trap,
+        mic,
+        coop,
+        estacionou,
+        alianca: alianca.value,
+        Key: Key.value,
+        totalValue: totalValue,
+        rankingPoints
+    };
+
+    try {
+        const response = await fetch('https://scout-mt.onrender.com/data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': 'PAOCOMCARNE123$',
+                'key': Key.value
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (!response.ok) {
+            throw new Error('Erro de internet');
+        }
+
+        const result = await response.json();
+        console.log('Enviado com sucesso:', result);
+        sucesso.style.display = 'block';
+    } catch (error) {
+        console.error('Deu ruim ao enviar:', error);
+        erro.style.display = 'block';
+    }
+
+    setTimeout(() => {
+        location.reload();
+    }, 7000);
+}
+
+// Adicionar evento ao botão de envio
+btnEnviar.addEventListener('click', sendData);
